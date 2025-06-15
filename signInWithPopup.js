2783ac2933577fb53a424dced7e76a9b8edbe0c1
nperamur <nperamur@gmail.com>
2025-06-14 22:10:46 -0700

    Update signInWithPopup.js

--- signInWithPopup.js
+++ signInWithPopup.js
@@ -1,24 +1,28 @@
-import { signInWithPopup, GoogleAuthProvider, getAuth } from'firebase/auth';
-import { initializeApp } from 'firebase/app';
-import firebaseConfig from './firebaseConfig.js'
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
+const firebaseConfig = {
+  apiKey: "AIzaSyB_Q4URffEbtM_Amrr4uVhqIDZeJeHTgNs",
+  authDomain: "my-page-summarizer.firebaseapp.com",
+  projectId: "my-page-summarizer",
+  storageBucket: "my-page-summarizer.firebasestorage.app",
+  messagingSenderId: "163674124159",
+  appId: "1:163674124159:web:a7df11133cf80ef4f0744a",
+  measurementId: "G-548ZGYP3H0"
+};
 
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
 
 const PARENT_FRAME = document.location.ancestorOrigins[0];
-
-
-const PROVIDER = new GoogleAuthProvider();
+const PROVIDER = new firebase.auth.GoogleAuthProvider();
 
 function sendResponse(result) {
   globalThis.parent.self.postMessage(JSON.stringify(result), PARENT_FRAME);
 }
 
-globalThis.addEventListener('message', function({data}) {
+globalThis.addEventListener('message', function({ data }) {
   if (data.initAuth) {
-    signInWithPopup(auth, PROVIDER)
+    auth.signInWithPopup(PROVIDER)
       .then(sendResponse)
-      .catch(sendResponse)
+      .catch(sendResponse);
   }
 });
